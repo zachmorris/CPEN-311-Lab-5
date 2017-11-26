e0e015e48654465342fcf8909a903f554f0b5b72
zachmorris <zachmorris23@gmail.com>
2017-11-25 16:08:40 -0800

    Initial commit
    Hard to decide what to keep, since there's a lot of generated files from
    Eclipse. Decided to keep bare minimum!

---
+++ scratch/LFSR/L5_LFSR.sv
@@ -0,0 +1,14 @@
+module L5_LFSR(input logic clk,
+					input logic reset,
+					output logic [4:0] q);
+
+logic feedback;					
+					
+always_ff @(posedge clk, posedge reset)
+	if(reset)	q <= 5'h1F;  // load with ones
+	else 			q <= {feedback, q[4:1]};
+
+assign feedback = q[0]^q[2];
+
+endmodule
+					
