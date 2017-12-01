75987587f8cb39ef6cf6d2c40ff4e5a59ba02a58
zachmorris <zachmorris23@gmail.com>
2017-11-30 16:20:04 -0800

    Added clock_domain simulation
    Multi-bit clock domain synchronization

--- scratch/clock_domain_1b/clock_domain_1b.sv
+++ scratch/clock_domain_1b/clock_domain_1b.sv
@@ -7,9 +7,8 @@ module clock_domain_1b
 								output logic en_5, reset_1,
 								output logic [width-1:0] sync_data_out);
 
-//reg q1, q2, q3, q4, q5;
-//logic en_5;
-
+// note: you can simulate a module that depends on parameters, just change 
+// 		variable width in the .vwf file
 
 assign reset_1 = ~slow_clk & q3;
 assign en_5 = q3 & ~q4;
